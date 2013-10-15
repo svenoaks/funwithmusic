ca44f9a69cfa41cb116147e6f22065be90fdb158
svenoaks <svenoaks@gmail.com>
2013-10-14 21:45:38 -0400

    3 activities

--- funwithmusic/gen/com/smp/funwithmusic/R.java
+++ funwithmusic/gen/com/smp/funwithmusic/R.java
@@ -27,7 +27,9 @@ public final class R {
         public static final int action_settings=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_john=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_steve=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -36,6 +38,8 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int johns_activity=0x7f050003;
+        public static final int steves_activity=0x7f050004;
     }
     public static final class style {
         /** 
--- funwithmusic/src/com/smp/funwithmusic/MainActivity.java
+++ funwithmusic/src/com/smp/funwithmusic/MainActivity.java
@@ -2,7 +2,9 @@ package com.smp.funwithmusic;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity
 {
@@ -20,6 +22,16 @@ public class MainActivity extends Activity
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void onSteveClicked(View view)
+	{
+		startActivity(new Intent(this, SteveActivity.class));
+	}
+	
+	public void onJohnClicked(View view)
+	{
+		startActivity(new Intent(this, JohnActivity.class));
 	}
 
 }
