0294340191796b6479ea3020b44750b7d0371051
svenoaks <svenoaks@gmail.com>
2013-10-15 20:21:12 -0400

    add song info to activity in cardUI

---
+++ funwithmusic/src/com/smp/funwithmusic/Constants.java
@@ -0,0 +1,8 @@
+package com.smp.funwithmusic;
+
+public class Constants
+{
+	public static final String SONG_ACTION = "com.smp.funwithmusic.SONG_ACTION";
+	public static final String SHARED_PREF_NAME = "com.smp.funwithmusic.PREFS";
+	public static final String SONG_FILE_NAME = "com.smp.funwithmusic.SONG_FILE_NAME";
+}
