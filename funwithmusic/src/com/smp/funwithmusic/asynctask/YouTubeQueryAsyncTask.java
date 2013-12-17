d2aa9092c6d1179f0184598fd798343d03a1fd23
Steve <svenoaks@gmail.com>
2013-12-16 19:23:45 -0500

    more youtube stability

--- funwithmusic/src/com/smp/funwithmusic/asynctask/YouTubeQueryAsyncTask.java
+++ funwithmusic/src/com/smp/funwithmusic/asynctask/YouTubeQueryAsyncTask.java
@@ -21,14 +21,13 @@ import com.google.api.services.youtube.model.SearchListResponse;
 import com.google.api.services.youtube.model.SearchResult;
 import com.smp.funwithmusic.global.GlobalRequest;
 
-
 public class YouTubeQueryAsyncTask extends AsyncTask<String, Void, List<SearchResult>>
 {
 	private static final HttpTransport HTTP_TRANSPORT = new ApacheHttpTransport();
 	private static final JsonFactory JSON_FACTORY = new JacksonFactory();
 	private static final long NUMBER_OF_VIDEOS_RETURNED = 25;
 	private static YouTube youtube;
-	
+
 	static
 	{
 		youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer()
@@ -40,7 +39,7 @@ public class YouTubeQueryAsyncTask extends AsyncTask<String, Void, List<SearchRe
 	}
 
 	private OnYoutubeSearchResults listener;
-	
+
 	public YouTubeQueryAsyncTask(OnYoutubeSearchResults listener)
 	{
 		this.listener = listener;
@@ -49,41 +48,44 @@ public class YouTubeQueryAsyncTask extends AsyncTask<String, Void, List<SearchRe
 	protected List<SearchResult> doInBackground(String... args)
 	{
 		List<SearchResult> searchResultList = null;
-		
-		try
-		{
-			
-			String queryTerm = args[0];
-			YouTube.Search.List search = youtube.search().list("id,snippet");
 
-			String apiKey = API_KEY_BROWSER_YOUTUBE;
-			search.setKey(apiKey);
-			search.setQ(queryTerm);
-			
-			search.setType("video");
-			
-			search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/high/url)");
-			search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
-			SearchListResponse searchResponse = search.execute();
+		if (listener != null &&
+				isOnline((Context) listener))
+			try
+			{
 
-			searchResultList = searchResponse.getItems();
-		}
-		catch (GoogleJsonResponseException e)
-		{
-			e.printStackTrace();
-			
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		
+				String queryTerm = args[0];
+				YouTube.Search.List search = youtube.search().list("id,snippet");
+
+				String apiKey = API_KEY_BROWSER_YOUTUBE;
+				search.setKey(apiKey);
+				search.setQ(queryTerm);
+
+				search.setType("video");
+
+				search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/high/url)");
+				search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
+				SearchListResponse searchResponse = search.execute();
+
+				searchResultList = searchResponse.getItems();
+			}
+			catch (GoogleJsonResponseException e)
+			{
+				e.printStackTrace();
+
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+
 		return searchResultList;
 	}
 
 	protected void onPostExecute(List<SearchResult> result)
 	{
-		if (listener != null) listener.onSearchResultsReceived(result);
+		if (listener != null)
+			listener.onSearchResultsReceived(result);
 	}
 
 	public void releaseReference()
